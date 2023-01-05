af1f948eb28fc64c239da846d5b628bcb1b1215e
Dahlia <rlally89@gmail.com>
2023-01-05 09:06:03 -0800

    BucketItem button group updated

--- src/components/BucketItem.js
+++ src/components/BucketItem.js
@@ -6,11 +6,9 @@ export default function BucketItem(props) {
 
     return (
         <List>
-            <ListItem>
+            <ListItem sx={{color: 'purple'}}>
                 {props.task} 
-
-                
-                <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                <ButtonGroup variant="text" aria-label="outlined button group">
                     <Button>Delete</Button>
                     <Button>Edit</Button>
                     <Button>Completed</Button>
